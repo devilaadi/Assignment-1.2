0bca9bd34d2e0e57c0ad61a5c8c28f1227deb7be
AdityaB <AdityaB@SDGL810.sdgc.com>
2017-06-22 03:00:32 +0530

    First Commit

---
+++ src/Assignment1_2.java
@@ -0,0 +1,26 @@
+
+public class Assignment1_2 {
+
+	public static void main(String[] args) {
+		
+		int a = 20;
+		int b = 10;
+		
+		 b = (a--) + (--a);
+		 
+		 System.out.println(b);
+		 
+		 int c = a--;
+		 System.out.println(c);
+		 
+		 
+		int d  = a>>2;
+		System.out.println(d);
+		
+		int e = a&b;
+		System.out.println(e);
+		
+
+	}
+
+}
